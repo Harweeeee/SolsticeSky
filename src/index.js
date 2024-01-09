5f6e150682d0df911ab473e7dfed306664b5f56c
Harweeeee <harlz31@hotmail.com>
2024-01-09 10:58:48 +1000

    Added Axios to head

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 function displayCity(event) {
     event.preventDefault();
-    let city = document.querySelector("h1");
+    let cityHeading = document.querySelector("h1");
     let searchValue = document.querySelector("#searchInput").value;
-    city.innerHTML = searchValue;
+    cityHeading.innerHTML = searchValue;
 }
 
 let searchClick = document.querySelector("#searchButton");
 searchClick.addEventListener("click", displayCity);
+
+const apiKey = "263tdf1c5f75o8ab465cedabbe0f4604";
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${searchValue}&key=${apiKey}&units=metric";
