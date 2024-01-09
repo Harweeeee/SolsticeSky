ae22370638ca6feb893dc6c46543f417229a730b
Harweeeee <harlz31@hotmail.com>
2024-01-09 11:30:07 +1000

    Added API and created temperature function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+const apiKey = "263tdf1c5f75o8ab465cedabbe0f4604";
+
+let searchClick = document.querySelector("#searchButton");
+searchClick.addEventListener("click", displayCity);
+
 function displayCity(event) {
     event.preventDefault();
     let cityHeading = document.querySelector("h1");
     let searchValue = document.querySelector("#searchInput").value;
     cityHeading.innerHTML = searchValue;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchValue}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);    
 }
 
-let searchClick = document.querySelector("#searchButton");
-searchClick.addEventListener("click", displayCity);
+function displayTemperature(response) {
+    let temperature = document.querySelector("#temperature");
+    let currentTemperature = Math.round(response.data.temperature.current);
+    temperature.innerHTML = `${currentTemperature}°C`;
+    console.log(response);
+}
 
-const apiKey = "263tdf1c5f75o8ab465cedabbe0f4604";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${searchValue}&key=${apiKey}&units=metric";
